8abcee9f0669e6ed9c4ec79a9db2015c4d2c3659
Daniel Pires <danielpires6@hotmail.com>
2020-08-05 11:54:31 -0300

    Adicionado API para buscar tipos de pokemons

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const pokemonsMock = require('./mock_pokemon');
 
 app.use(bodyParser.json());
 
+app.get('/api/v1/pokemons/types', function(req, res) {
+  let mock = pokemonsMock;
+
+  mock = mock.map(pokemon => {
+    return pokemon.type;
+  })
+
+  console.log(mock);
+
+  mock = [...new Set(mock)];
+
+  res.status(200).json({
+    pokemons: mock
+  })
+})
+
 app.get('/api/v1/pokemons/:type', function(req, res) {
 
   let type = req.params.type
@@ -33,11 +49,14 @@ app.get('/api/v1/trainer/:trainer/pokemons', function(req, res) {
 
 })
 
+
 app.get('/api/v1/pokemons', function(req, res) {
   res.status(200).json({
     pokemons: pokemonsMock
   })
 })
+
+
 
 app.listen(8000, function() {
   console.log('Servidor rodando na porta 8000.');
